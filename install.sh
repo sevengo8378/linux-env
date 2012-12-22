f1fba7650239424379bc38b495cf4a29445b464e
byrne.liang <byrne.liang@saybot.com>
2012-12-22 13:11:42 +0800

    add hosts install

--- install.sh
+++ install.sh
@@ -2,8 +2,18 @@
 
 cwd=`pwd`
 
+echo "cp $cwd/bashrc $HOME/.bashrc"
 cp $cwd/bashrc $HOME/.bashrc
+
+echo "cp $cwd/profile $HOME/.profile"
 cp $cwd/profile $HOME/.profile
+
+echo "cp $cwd/gitconfig $HOME/.gitconfig"
 cp $cwd/gitconfig $HOME/.gitconfig
+
+echo "cp $cwd/tmux.config $HOME/.tmux.conf"
 cp $cwd/tmux.config $HOME/.tmux.conf
 
+#echo "install hosts to /etc/hosts"
+#sudo cp /etc/hosts{,_bak} 
+#sudo cp $cwd/hosts /etc/hosts
