5ff6fd46a5f485226c5e8a3d90f6609d847f9e60
byrne.liang <byrne.liang@saybot.com>
2013-05-12 20:59:27 +0800

    增加vim,ohmyzsh的安装,自动切换shell到zsh，自动安装homebrew，完善readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-run install.sh
+this project is helping you config linux development enviroment quickly.
+
+before install make sure you have installed the dependencies below.
+1. git
+
+install
+./install.sh
--- install.sh
+++ install.sh
@@ -2,6 +2,13 @@
 
 cwd=`pwd`
 
+# clone some git repositories to $HOME
+cd $HOME
+git clone git@github.com:byrne/oh-my-zsh.git .oh-my-zsh
+git clone git@github.com:byrne/vimfiles.git .vim
+ln -sf $HOME/.vim/vimrc.improve $HOME/.vimrc
+
+cd $cwd
 echo "cp $cwd/bashrc $HOME/.bashrc"
 cp $cwd/bashrc $HOME/.bashrc
 
@@ -23,3 +30,11 @@ cp $cwd/zshrc $HOME/.zshrc
 
 is_mac=`uname | grep Darwin`
 [ -n $is_mac ] && cp $cwd/show-hide-files ~/bin/
+
+cd $HOME
+# switch shell to zsh
+chsh -s /bin/zsh
+. $HOME/.zshrc
+
+# install homebrew
+#ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
